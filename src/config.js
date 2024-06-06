ed47e0d513a0f8b152327f95ffbe752aba81eb5f
thakuhuni <36413887+thakuhuni@users.noreply.github.com>
2024-06-06 22:06:38 +1000

    first commit

---
+++ src/config.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018-Present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+const CLIENT_ID = process.env.CLIENT_ID || '0oahfq3ywe8hduBze5d7';
+const ISSUER = process.env.ISSUER || 'https://dev-4216231.okta.com/oauth2/default';
+const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+const BASENAME = import.meta.env.BASE_URL || '';
+// BASENAME includes trailing slash
+const REDIRECT_URI = `${window.location.origin}${BASENAME}login/callback`;
+
+export default {
+  oidc: {
+    clientId: CLIENT_ID,
+    issuer: ISSUER,
+    redirectUri: REDIRECT_URI,
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+  },
+  resourceServer: {
+    messagesUrl: 'http://127.0.0.1:4173/messages',
+    
+  },
+  app: {
+    basename: BASENAME,
+  },
+};
